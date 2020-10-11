5570f681fffb9d974de5fad515ae5e9c1de9b8f5
Alex Sullivan <alexsullivan114@gmail.com>
2020-10-11 12:47:36 -0400

    Try and fix an issue where our settings wouldn't be reset after midnight

--- app/src/main/java/com/alexsullivan/datacollor/CollectorWidget.kt
+++ app/src/main/java/com/alexsullivan/datacollor/CollectorWidget.kt
@@ -14,24 +14,23 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.util.*
 
-private val states = mutableMapOf<Trackable, Boolean>().apply {
-    for (trackable in Trackable.values()) {
-        put(trackable, false)
-    }
-}
-
 class CollectorWidget : AppWidgetProvider() {
 
+    private var trackableManager: TrackableManager? = null
 
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // There may be multiple widgets active, so update all of them
-        for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+        GlobalScope.launch {
+            getTrackingManager(context).update()
+            // There may be multiple widgets active, so update all of them
+            for (appWidgetId in appWidgetIds) {
+                updateAppWidget(context, appWidgetManager, appWidgetId)
+            }
         }
+
     }
 
     override fun onEnabled(context: Context) {
@@ -46,26 +45,10 @@ class CollectorWidget : AppWidgetProvider() {
         super.onReceive(context, intent)
         if (Trackable.values().map { it.name }.contains(intent.action)) {
             val trackable = Trackable.values().first { it.name == intent.action }
-            states[trackable] = !states.getValue(trackable)
-            val ids: IntArray = AppWidgetManager.getInstance(context).getAppWidgetIds(ComponentName(context, CollectorWidget::class.java))
-            onUpdate(context, AppWidgetManager.getInstance(context), ids)
-
-            val calendar: Calendar = GregorianCalendar()
-            calendar[Calendar.HOUR_OF_DAY] = 0
-            calendar[Calendar.MINUTE] = 0
-            calendar[Calendar.SECOND] = 0
-            calendar[Calendar.MILLISECOND] = 0
-            val date = calendar.time
-            val trackableEntity = TrackableEntity(trackable, states.getValue(trackable), date)
-
             GlobalScope.launch {
-                withContext(Dispatchers.IO) {
-                    TrackableEntityDatabase.getDatabase(context).trackableEntityDao()
-                        .saveEntity(trackableEntity)
-                    val entities = TrackableEntityDatabase.getDatabase(context).trackableEntityDao()
-                        .getTrackableEntities()
-                    Log.d("entities", "Entities: $entities")
-                }
+                getTrackingManager(context).toggle(trackable)
+                val ids: IntArray = AppWidgetManager.getInstance(context).getAppWidgetIds(ComponentName(context, CollectorWidget::class.java))
+                onUpdate(context, AppWidgetManager.getInstance(context), ids)
             }
         }
     }
@@ -81,19 +64,28 @@ class CollectorWidget : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetId: Int
     ) {
-        val remoteViews = RemoteViews(context.packageName, R.layout.collector_widget)
-        remoteViews.removeAllViews(R.id.grid)
-        for (trackable in Trackable.values()) {
-            val checkbox =
-                if (states.getValue(trackable)) R.drawable.ic_baseline_check_box_24 else R.drawable.ic_baseline_check_box_outline_blank_24
-            val item = RemoteViews(context.packageName, R.layout.trackable_item)
-            remoteViews.addView(R.id.grid, item)
-            item.setTextViewText(R.id.text, trackable.title)
-            item.setImageViewResource(R.id.check, checkbox)
-            item.setOnClickPendingIntent(R.id.check, getPendingSelfIntent(context, trackable.name))
+        GlobalScope.launch {
+            val remoteViews = RemoteViews(context.packageName, R.layout.collector_widget)
+            remoteViews.removeAllViews(R.id.grid)
+            for (entry in getTrackingManager(context).state) {
+                val checkbox = if (entry.value) R.drawable.ic_baseline_check_box_24 else R.drawable.ic_baseline_check_box_outline_blank_24
+                val item = RemoteViews(context.packageName, R.layout.trackable_item)
+                remoteViews.addView(R.id.grid, item)
+                item.setTextViewText(R.id.text, entry.key.title)
+                item.setImageViewResource(R.id.check, checkbox)
+                item.setOnClickPendingIntent(R.id.check, getPendingSelfIntent(context, entry.key.name))
+            }
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
         }
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+    }
+
+    private suspend fun getTrackingManager(context: Context): TrackableManager {
+        if (trackableManager == null) {
+            trackableManager = TrackableManager(TrackableEntityDatabase.getDatabase(context))
+            trackableManager!!.init()
+        }
+        return trackableManager!!
     }
 }
 
--- app/src/main/java/com/alexsullivan/datacollor/TrackableEntityDao.kt
+++ app/src/main/java/com/alexsullivan/datacollor/TrackableEntityDao.kt
@@ -1,6 +1,7 @@
 package com.alexsullivan.datacollor
 
 import androidx.room.*
+import java.util.*
 
 @Dao
 interface TrackableEntityDao {
@@ -9,6 +10,9 @@ interface TrackableEntityDao {
 
     @Query("SELECT * FROM trackable_entity_table")
     suspend fun getTrackableEntities(): List<TrackableEntity>
+
+    @Query("SELECT * FROM trackable_entity_table WHERE date = :date")
+    suspend fun getTrackableEntities(date: Date): List<TrackableEntity>
 
     @Query("DELETE FROM trackable_entity_table")
     suspend fun nuke()
