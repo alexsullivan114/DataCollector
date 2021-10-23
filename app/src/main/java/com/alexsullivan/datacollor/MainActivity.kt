dfbf8bd228f919fb29bf35f3a60cec55214e5e86
Alex Sullivan <alexsullivan114@gmail.com>
2021-10-23 18:12:42 +0545

    Move out to separate composables

--- app/src/main/java/com/alexsullivan/datacollor/MainActivity.kt
+++ app/src/main/java/com/alexsullivan/datacollor/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.alexsullivan.datacollor
 
 import android.Manifest
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
@@ -10,23 +9,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.alexsullivan.datacollor.database.Trackable
 import com.alexsullivan.datacollor.database.TrackableEntityDatabase
 import com.alexsullivan.datacollor.database.TrackableManager
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.io.File
-import java.io.FileWriter
 
 
 class MainActivity : AppCompatActivity() {
@@ -49,34 +44,49 @@ class MainActivity : AppCompatActivity() {
         requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
         lifecycleScope.launch {
             setContent {
-                val trackables by viewModel.itemsFlow.collectAsState()
-                LazyColumn(modifier = Modifier.fillMaxWidth()) {
-                    trackables.forEach { trackable ->
-                        item {
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(16.dp),
-                                verticalAlignment = Alignment.CenterVertically,
-                                horizontalArrangement = Arrangement.SpaceBetween
-                            ) {
-                                Text(trackable.title)
-                                Checkbox(checked = trackable.enabled, onCheckedChange = { checked ->
-                                    viewModel.trackableToggled(trackable, checked)
-                                })
-                            }
-                            Divider()
-                        }
-                    }
-                    item {
-                        Button(modifier = Modifier
-                            .padding(16.dp)
-                            .fillMaxWidth(), onClick = { export() }) {
-                            Text("Export")
-                        }
-                    }
+                TrackableList()
+            }
+        }
+    }
+
+    @Composable
+    fun TrackableList(modifier: Modifier = Modifier) {
+        val trackables by viewModel.itemsFlow.collectAsState()
+        LazyColumn(modifier = modifier.fillMaxWidth()) {
+            trackables.forEach { trackable ->
+                item {
+                    TrackableItem(trackable)
                 }
             }
+            item {
+                ExportButton()
+            }
+        }
+    }
+
+    @Composable
+    fun TrackableItem(trackable: Trackable, modifier: Modifier = Modifier) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(trackable.title)
+            Checkbox(checked = trackable.enabled, onCheckedChange = { checked ->
+                viewModel.trackableToggled(trackable, checked)
+            })
+        }
+        Divider()
+    }
+
+    @Composable
+    fun ExportButton(modifier: Modifier = Modifier) {
+        Button(modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth(), onClick = { export() }) {
+            Text("Export")
         }
     }
 
