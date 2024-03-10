389af5e14941d04fbcd9f1abe106fa5088939775
Alex Sullivan <alexsullivan114@gmail.com>
2024-03-09 21:30:19 -0500

    Move configuration activity to new screen composable structure

--- app/src/main/java/com/alexsullivan/datacollor/configuration/ConfigurationViewModel.kt
+++ app/src/main/java/com/alexsullivan/datacollor/configuration/ConfigurationViewModel.kt
@@ -1,10 +1,12 @@
 package com.alexsullivan.datacollor.configuration
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.alexsullivan.datacollor.UpdateTrackablesUseCase
 import com.alexsullivan.datacollor.database.Trackable
 import com.alexsullivan.datacollor.database.TrackableManager
+import com.alexsullivan.datacollor.utils.refreshWidget
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -17,7 +19,8 @@ import javax.inject.Inject
 @HiltViewModel
 class ConfigurationViewModel @Inject constructor(
     private val trackableManager: TrackableManager,
-    private val updateTrackablesUseCase: UpdateTrackablesUseCase
+    private val updateTrackablesUseCase: UpdateTrackablesUseCase,
+    private val application: Application
 ) : ViewModel() {
     private val _itemsFlow = MutableStateFlow(emptyList<Trackable>())
     private val _triggerUpdateWidgetsFlow = Channel<Unit>()
@@ -32,6 +35,12 @@ class ConfigurationViewModel @Inject constructor(
                     _itemsFlow.emit(it)
                     _triggerUpdateWidgetsFlow.send(Unit)
                 }
+        }
+
+        viewModelScope.launch {
+            triggerUpdateWidgetFlow.collect {
+                refreshWidget(application)
+            }
         }
     }
 
