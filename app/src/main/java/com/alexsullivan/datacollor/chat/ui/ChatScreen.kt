b2d06b0a762c4d1324a8eba38faf24a94fd373ac
Alex Sullivan <alexsullivan114@gmail.com>
2024-03-18 19:49:13 -0400

    Scroll to bottom when a new message comes in

--- app/src/main/java/com/alexsullivan/datacollor/chat/ui/ChatScreen.kt
+++ app/src/main/java/com/alexsullivan/datacollor/chat/ui/ChatScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,10 +20,17 @@ fun ChatScreen() {
     val viewModel = hiltViewModel<ChatViewModel>()
     Column(modifier = Modifier.padding(16.dp)) {
         val messages by viewModel.messages.collectAsStateWithLifecycle(initialValue = emptyList())
+        val chatListState = rememberLazyListState()
+
+        LaunchedEffect(key1 = messages) {
+            chatListState.animateScrollToItem(messages.size)
+        }
+
         LazyColumn(
             modifier = Modifier
                 .fillMaxWidth()
-                .weight(1.0f)
+                .weight(1.0f),
+            state = chatListState
         ) {
             messages.forEach {
                 item(key = it.id) {
